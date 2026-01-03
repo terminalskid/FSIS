663a126c72dbb190fe27320050e02ec209e3ff6f
SkidBoi <skid.fanning788@slmails.com>
2026-01-03 00:43:48 +0000

    Update fsis.ps1

--- fsis.ps1
+++ fsis.ps1
@@ -62,9 +62,7 @@ $portableDirs = @(
 foreach ($dir in $portableDirs) {
     if (Test-Path $dir) {
         Get-ChildItem $dir -Directory -Depth 2 -ErrorAction SilentlyContinue | ForEach-Object {
-            if (
-                Get-ChildItem $_.FullName -File -Include *.exe,*.bat,*.ps1 -ErrorAction SilentlyContinue
-            ) {
+            if (Get-ChildItem $_.FullName -File -Include *.exe,*.bat,*.ps1 -ErrorAction SilentlyContinue) {
                 Add-App $_.Name "" "Portable" $_.FullName
             }
         }
